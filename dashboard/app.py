7d700b3cb6647fd70ea6cfba5ba22d41f4e5e22a
Haim-hub <niklas@haim.dk>
2025-09-29 13:35:41 +0200

    multiple pages are not cool in shiny

--- dashboard/app.py
+++ dashboard/app.py
@@ -3,13 +3,13 @@ import re
 from faicons import icon_svg
 
 # Import data from shared.py
-from shared import app_dir, df, pen_df
+from shared import app_dir, df, salary_df
 
 from shiny import reactive
 from shiny.express import input, render, ui
 import matplotlib.ticker as ticker
 
-ui.page_opts(title="Tech Salaries dashboard", fillable=True)
+ui.page_opts(title="UFO Sightings dashboard", fillable=True)
 
 
 
@@ -22,83 +22,74 @@ ui.page_opts(title="Tech Salaries dashboard", fillable=True)
 #        selected=["Adelie", "Gentoo", "Chinstrap"],
 #    )
 
+with ui.navset_pill(id="tab"):  
+    with ui.nav_panel("A"):
+        with ui.layout_column_wrap(fill=False):
+            with ui.value_box(showcase=icon_svg("satellite-dish")):
+                "Number of Sightings"
 
-with ui.layout_column_wrap(fill=False):
-    with ui.value_box(showcase=icon_svg("users-line")):
-        "Number of people"
+                @render.text
+                def count():
+                    return filtered_df().shape[0]
+                
 
-        @render.text
-        def count():
-            return filtered_df().shape[0]
+            with ui.value_box(showcase=icon_svg("hourglass-start")):
+                "Average Length of Encounter in Seconds"
 
-    with ui.value_box(showcase=icon_svg("business-time")):
-        "Average Total Experience Years"
+                @render.text
+                def bill_length():
+                    return f"{filtered_df()['length_of_encounter_seconds'].mean():.1f} seconds"
+                
+            with ui.value_box(showcase=icon_svg("calendar-days")):
+                "Average Sightings Per Year"
 
-        @render.text
-        def bill_length():
-            return f"{filtered_df()['total_experience_years'].mean():.1f} years"
-
-    with ui.value_box(showcase=icon_svg("money-bill")):
-        "Average Annual Base Pay"
-
-        @render.text
-        def bill_depth():
-            return f"{filtered_df()['annual_base_pay'].mean():.1f} $"
+                @render.text
+                def bill_depth():
+                    return f"{year_df()['Unnamed: 0'].mean():.1f} Sightings"
 
 
-with ui.layout_columns():
-    with ui.card(full_screen=True):
-        ui.card_header("Experience and Anual Base Pay")
+        with ui.layout_columns():
+            with ui.card(full_screen=True):
+                ui.card_header("Experience and Anual Base Pay")
 
-        @render.plot
-        def length_depth():
-            ax = sns.scatterplot(
-                data=filtered_df(),
-                x="total_experience_years",
-                y="annual_base_pay",
-                hue="job_title_category",
-            )
-            # Force decimal notation
-            ax.yaxis.set_major_formatter(ticker.ScalarFormatter(useOffset=False))
-            ax.ticklabel_format(style='plain', axis='y')
-            return ax
+                @render.plot
+                def length_depth():
+                    ax = sns.scatterplot(
+                        data=year_df(),
+                        x="Year",
+                        y="Unnamed: 0"
+                    )
+                    # Force decimal notation
+                    ax.yaxis.set_major_formatter(ticker.ScalarFormatter(useOffset=False))
+                    ax.ticklabel_format(style='plain', axis='y')
+                    return ax
 
-    with ui.card(full_screen=True):
-        ui.card_header("Tech Salary data")
+            with ui.card(full_screen=True):
+                ui.card_header("UFO Sightings Data")
 
-        @render.data_frame
-        def summary_statistics():
-            cols = [
-                "job_title",
-                "job_title_category",
-                "total_experience_years",
-                "annual_base_pay",
-            ]
-            return render.DataGrid(filtered_df()[cols], filters=True)
+                @render.data_frame
+                def summary_statistics():
+                    cols = [
+                        "Date_time",
+                        "Country",
+                        "Region",
+                        "UFO_shape",
+                        "length_of_encounter_seconds",
+                    ]
+                    return render.DataGrid(filtered_df()[cols], filters=True)
+                
+    with ui.nav_panel("B"):
+        "This is the second 'page'."
 
 
 ui.include_css(app_dir / "styles.css")
 
-
-@reactive.calc
-def filtered_pen_df():
-    filt_df = pen_df[pen_df["species"].isin(input.species())]
-    filt_df = filt_df.loc[filt_df["body_mass_g"] < input.mass()]
-    return filt_df
-
-def is_valid_job_title(name):
-    # Check for excessive special characters or numbers
-    if re.search(r'[!@#$%^&*()_+=\[\]{};:\'"\\|,.<>\/?0-9ÛÜûüæøåÆØÅ]{3,}', name):
-        return False
-    # Check for unreasonable length
-    if len(name) < 3 or len(name) > 100:
-        return False
-    if name in ["nigger in cheif", "asdf", "_à¾á±á´¬ÜüÇ", "tom", "twat master", "muffin stuffer", "chief lubricator", "sveeeeeeeeetrs peeeeeeeeetrs", "buch", "job title"]:
-        return False
-    return True
-
 @reactive.calc
 def filtered_df():
-    filt_df = df[df['job_title'].apply(is_valid_job_title)]
-    filt_df = filt_df[filt_df["annual_base_pay"] < 1000000]
+    filt_df = df
+    return filt_df
+
+@reactive.calc
+def year_df():
+    filt_df = df.groupby(by="Year").count()
     return filt_df
